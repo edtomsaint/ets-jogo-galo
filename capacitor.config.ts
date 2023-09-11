81511ac5117bef5b0df53c154650b84c9c1679d3
edtomsaint <edtomsaint@gmail.com>
2023-09-11 12:20:34 +0100

    initial

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ets-jogo-galo',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
